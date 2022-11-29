0e3622319edb9cfb4e9c6bd845fbdcce2593b7f4
TwistMart <martineelucky1996@gmail.com>
2022-11-29 16:59:30 +0300

    first commit

---
+++ lib/screens/test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test')      
+      ),
+      body: Text('Hello Carmelita'),
+    );
+  }
+}
---
+++ lib/widgets/items.widget.dart
@@ -0,0 +1,120 @@
+import 'package:coffee_shop/screens/single_item_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ItemsWidget extends StatelessWidget {
+  List img = [
+    'Latte',
+    'Espresso',
+    'Black Coffee',
+    'Cold Coffee',
+  ];
+  ItemsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      physics: NeverScrollableScrollPhysics(),
+      childAspectRatio: (150 / 195),
+      shrinkWrap: true,
+      children: [
+        for (int i = 0; i < img.length; i++)
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color(0xFF212325),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.4),
+                  spreadRadius: 1,
+                  blurRadius: 8,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SingleItemScreen(img[i]),
+                        ));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/${img[i]}.png",                      
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          img[i],
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white60,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "Best coffee",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "\$30",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Color(0xFFE57734),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Icon(
+                          CupertinoIcons.add,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+}
